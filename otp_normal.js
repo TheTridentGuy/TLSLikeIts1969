f94db632e8950a8b24481715f14e021f1bcee197
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-05-21 20:59:39 -0700

    final touches for today, gonna work on generation tomorrow

--- otp_normal.js
+++ otp_normal.js
@@ -4,7 +4,6 @@ import { get_otp_name } from "./otp_lib.js";
 const $ = (selector) => document.querySelector(selector);
 
 
-console.log("otp_normal.js loaded");
 function generate_otp(){
     $("#otp-name").innerHTML = get_otp_name();
 }
