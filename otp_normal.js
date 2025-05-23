fa16475b391ff7ca78b7a9dd39e720f1c6e7620b
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-05-22 22:26:31 -0700

    start on generation code

--- otp_normal.js
+++ otp_normal.js
@@ -1,4 +1,4 @@
-import { get_otp_name } from "./otp_lib.js";
+import { get_otp_name, crypto_safe_random_int } from "./otp_lib.js";
 
 
 const $ = (selector) => document.querySelector(selector);
@@ -6,6 +6,13 @@ const $ = (selector) => document.querySelector(selector);
 
 function generate_otp(){
     $("#otp-name").innerHTML = get_otp_name();
+    let otp_container = $("#otp");
+    for(let i = 0; i < $("#otp-nums").value; i++){
+        let num = document.createElement("span");
+        num.innerText = crypto_safe_random_int(0, 99).toString().padStart(2, "0");
+        num.className = "num";
+        otp_container.appendChild(num);
+    }
 }
 
 $("#print").addEventListener("click", function(){
