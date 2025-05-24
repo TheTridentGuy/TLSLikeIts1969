715db29ce7a32053cbffa8c135155a251e37455f
Aiden Bohlander <aiden.bohlander@gmail.com>
2025-05-23 19:25:28 -0700

    minor bug fixes

--- otp_normal.js
+++ otp_normal.js
@@ -5,11 +5,19 @@ const $ = (selector) => document.querySelector(selector);
 
 
 function generate_otp(){
+    let len = parseInt($("#otp-len").value);
+    let min = parseInt($("#otp-range-min").value);
+    let max = parseInt($("#otp-range-max").value);
+    if((isNaN(len) || isNaN(min) || isNaN(max) || min >= max) || len <= 0){
+        return;
+    }
     $("#otp-name").innerHTML = get_otp_name();
+    $("#otp-stats").innerHTML = `length: ${len}, range: [${min},${max}]`;
     let otp_container = $("#otp");
-    for(let i = 0; i < $("#otp-nums").value; i++){
+    otp_container.innerHTML = "";
+    for(let i = 0; i < len; i++){
         let num = document.createElement("span");
-        num.innerText = crypto_safe_random_int(0, 99).toString().padStart(2, "0");
+        num.innerText = crypto_safe_random_int(min, max).toString().padStart(2, "0");
         num.className = "num";
         otp_container.appendChild(num);
     }
